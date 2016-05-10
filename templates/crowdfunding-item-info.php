323a51b1a66bff83ccea1b586e8cb8a43f190560
Diego Ventura <diego@dventura.net>
2016-05-10 01:22:25 -0500

    currency format

--- templates/crowdfunding-item-info.php
+++ templates/crowdfunding-item-info.php
@@ -5,7 +5,7 @@
 ?>
 <div class="product-meta">
     <span class="price">
-        Precio Meta: <span class="amount">$<?=$crowd_funding_price_goal ?></span>
+        Precio Meta: <span class="amount">$<?=number_format($crowd_funding_price_goal, 2) ?></span>
     </span>
     <h6><?=$name ?></h6>
 </div>
